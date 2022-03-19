34473612fd6c02614ea0d33df4957be06e5dbf90
cobriensr <charles.a.obrien@outlook.com>
2022-03-19 12:16:57 -0400

    updated styling with tailwind forms

--- src/App.svelte
+++ src/App.svelte
@@ -12,22 +12,36 @@
 </script>
 
 <main>
-	<h1>Dollars to Cents Converter</h1>
-	<h2>Find the Smallest Amount of Coins Given Current Cents</h2>
-	<div id="inputBox">
-		<form id="dollarConvertForm" name="convert">
-			<label for="dollars">Dollar Amount to Convert:</label>
-			<input type="number" id="dollars" step=".01" min="0" placeholder=0 required bind:value={$dollarAmt}>
-			<label for="converted">Converted Amount in Cents:</label>
-			<input type="text" id="converted" name="outputBox" placeholder=0>
-		</form>
-	</div>
-	<div id="submitBtn">
-		<button form="dollarConvertForm" type="submit">Convert Dollars to Cents!</button>
-		<button id="coinConvert" type="button">Convert Cents to Coins!</button>
-	</div>
-	<div>
-		<p id="finalOutput"></p>
+	<div class="antialiased text-gray-900 px-6">
+		<div class="max-w-xl mx-auto py-12 divide-y md:max-w-4xl">
+			<div class="py-8">
+				<h1 class="text-4xl font-bold">Dollars to Cents Converter</h1>
+				<h2 class="mt-2 text-lg text-gray-600">Find the Smallest Amount of Coins Given Current Cents</h2>
+			</div>
+			<div class="py-12" id="inputBox">
+				<div class="mt-8 max-w-md">			
+					<form id="dollarConvertForm" name="convert">
+						<div class="grid grid-cols-1 gap-">
+							<label class="block" for="dollars">
+								<span class="text=gray-700">Dollar Amount to Convert:</span>
+								<input class="mt-1 block w-full" type="number" id="dollars" step=".01" min="0" placeholder=0 required bind:value={$dollarAmt}>
+							</label>
+							<label class="block" for="converted">
+								<span class="text-gray-700">Converted Amount in Cents:</span>
+								<input class="mt-1 block w-full" type="text" id="converted" name="outputBox" placeholder=0>
+							</label>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div id="submitBtn">
+				<button form="dollarConvertForm" type="submit">Convert Dollars to Cents!</button>
+				<button id="coinConvert" type="button">Convert Cents to Coins!</button>
+			</div>
+			<div>
+				<p id="finalOutput"></p>
+			</div>
+		</div>
 	</div>
 </main>
 
