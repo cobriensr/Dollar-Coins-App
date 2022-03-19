da83536ba9b87e0017a96e5e6140dff987849204
cobriensr <charles.a.obrien@outlook.com>
2022-03-18 23:55:32 -0400

    completed MVP and adding functionality for coin conversion feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { dollarAmt } from './stores/content';
 	import centsConvert from '../src/services/convertDollarsToCents';
+	import coinsConvert from '../src/services/convertCentsToCoins';
 
 	window.onload = () => {
 		const form = document.getElementById('dollarConvertForm');
-		form?.addEventListener('submit', centsConvert)
+		form?.addEventListener('submit', centsConvert);
+		const button = document.getElementById('coinConvert');
+		button?.addEventListener('click', coinsConvert);
 	};
 </script>
 
@@ -13,14 +16,15 @@
 	<h2>Find the Smallest Amount of Coins Given Current Cents</h2>
 	<div id="inputBox">
 		<form id="dollarConvertForm" name="convert">
-			<label for="dollars">Dollar Amount to Convert</label>
+			<label for="dollars">Dollar Amount to Convert:</label>
 			<input type="number" id="dollars" step=".01" min="0" placeholder=0 required bind:value={$dollarAmt}>
-			<label for="converted">Coin Converted Amount:</label>
+			<label for="converted">Converted Amount in Cents:</label>
 			<input type="text" id="converted" name="outputBox" placeholder=0>
 		</form>
 	</div>
 	<div id="submitBtn">
-		<button form="dollarConvertForm" type="submit">Convert!</button>
+		<button form="dollarConvertForm" type="submit">Convert Dollars to Cents!</button>
+		<button id="coinConvert" type="button">Convert Cents to Coins!</button>
 	</div>
 </main>
 
--- src/services/convertCentsToCoins.ts
+++ src/services/convertCentsToCoins.ts
@@ -1,1 +1,13 @@
 import { get } from 'svelte/store';
+import { dollarAmt } from '../stores/content';
+
+export default function coinsConvert (): void {
+    const dollarAmtValue: number = get(dollarAmt)
+    const result: string = convertCentstoCoins(dollarAmtValue)
+    const selection = document.getElementById('converted') as HTMLInputElement
+    selection.value = result
+}
+
+function convertCentstoCoins (value: number): string {
+    return ''
+}
